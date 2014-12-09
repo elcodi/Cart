solilokiam/elcodi

State Transitional Machine for Elcodi.
This is an implementation of a State Transitional Machine and the intergration
with given Elcodi implementation.

src/Elcodi/Component/Cart/Entity/Interfaces/OrderInterface.php
@@ -21,12 +21,13 @@ use Doctrine\Common\Collections\Collection;
 use Elcodi\Component\Currency\Entity\Interfaces\MoneyInterface;
 use Elcodi\Component\Geo\Entity\Interfaces\AddressInterface;
 use Elcodi\Component\Product\Entity\Interfaces\DimensionableInterface;
+use Elcodi\Component\StateTransitionMachine\Entity\Interfaces\StateableInterface;
 use Elcodi\Component\User\Entity\Interfaces\CustomerInterface;
 
 /**
  * Class OrderInterface
  */
-interface OrderInterface extends PriceInterface, DimensionableInterface
+interface OrderInterface extends PriceInterface, DimensionableInterface, StateableInterface
 {
     /**
      * Sets Customer
@@ -95,40 +96,6 @@ interface OrderInterface extends PriceInterface, DimensionableInterface
     public function removeOrderLine(OrderLineInterface $orderLine);
 
     /**
-     * Set order histories
-     *
-     * @param Collection $orderHistories Order histories
-     *
-     * @return $this self Object
-     */
-    public function setOrderHistories(Collection $orderHistories);
-
-    /**
-     * Get order histories
-     *
-     * @return Collection Order histories
-     */
-    public function getOrderHistories();
-
-    /**
-     * Add Order History
-     *
-     * @param OrderHistoryInterface $orderHistory Order History
-     *
-     * @return $this self Object
-     */
-    public function addOrderHistory(OrderHistoryInterface $orderHistory);
-
-    /**
-     * Remove Order History
-     *
-     * @param OrderHistoryInterface $orderHistory Order History
-     *
-     * @return $this self Object
-     */
-    public function removeOrderHistory(OrderHistoryInterface $orderHistory);
-
-    /**
      * Set quantity
      *
      * @param int $quantity Quantity
@@ -143,22 +110,6 @@ interface OrderInterface extends PriceInterface, DimensionableInterface
      * @return integer Quantity
      */
     public function getQuantity();
-
-    /**
-     * Sets LastOrderHistory
-     *
-     * @param OrderHistoryInterface $lastOrderHistory LastOrderHistory
-     *
-     * @return $this Self object
-     */
-    public function setLastOrderHistory(OrderHistoryInterface $lastOrderHistory);
-
-    /**
-     * Get LastOrderHistory
-     *
-     * @return OrderHistoryInterface LastOrderHistory
-     */
-    public function getLastOrderHistory();
 
     /**
      * Gets the Coupon amount with tax

src/Elcodi/Component/Cart/Entity/Interfaces/OrderLineInterface.php
@@ -16,17 +16,16 @@
 
 namespace Elcodi\Component\Cart\Entity\Interfaces;
 
-use Doctrine\Common\Collections\Collection;
-
 use Elcodi\Component\Product\Entity\Interfaces\DimensionableInterface;
 use Elcodi\Component\Product\Entity\Interfaces\ProductInterface;
 use Elcodi\Component\Product\Entity\Interfaces\PurchasableInterface;
 use Elcodi\Component\Product\Entity\Interfaces\VariantInterface;
+use Elcodi\Component\StateTransitionMachine\Entity\Interfaces\StateableInterface;
 
 /**
  * Class OrderLineInterface
  */
-interface OrderLineInterface extends PriceInterface, DimensionableInterface
+interface OrderLineInterface extends PriceInterface, DimensionableInterface, StateableInterface
 {
     /**
      * Set Order
@@ -107,56 +106,6 @@ interface OrderLineInterface extends PriceInterface, DimensionableInterface
      * @return integer Quantity
      */
     public function getQuantity();
-
-    /**
-     * Set order line histories
-     *
-     * @param Collection $orderLineHistories Order histories
-     *
-     * @return $this self Object
-     */
-    public function setOrderLineHistories(Collection $orderLineHistories);
-
-    /**
-     * Get order line histories
-     *
-     * @return Collection Order Line histories
-     */
-    public function getOrderLineHistories();
-
-    /**
-     * Add Order History
-     *
-     * @param OrderLineHistoryInterface $orderLineHistory Order History
-     *
-     * @return $this self Object
-     */
-    public function addOrderLineHistory(OrderLineHistoryInterface $orderLineHistory);
-
-    /**
-     * Remove Order History
-     *
-     * @param OrderLineHistoryInterface $orderLineHistory Order Line History
-     *
-     * @return $this self Object
-     */
-    public function removeOrderLineHistory(OrderLineHistoryInterface $orderLineHistory);
-
-    /**
-     * Sets LastOrderLineHistory
-     *
-     * @param OrderLineHistoryInterface $lastOrderLineHistory LastOrderLineHistory
-     *
-     * @return OrderLineInterface Self object
-     */
-    public function setLastOrderLineHistory(OrderLineHistoryInterface $lastOrderLineHistory);
-
-    /**
-     * Get LastOrderLineHistory
-     *
-     * @return OrderLineHistoryInterface LastOrderLineHistory
-     */
-    public function getLastOrderLineHistory();
 
     /**
      * Set the height

elcodi/Cart

Entity/Interfaces/OrderInterface.php
@@ -21,12 +21,13 @@ use Doctrine\Common\Collections\Collection;
 use Elcodi\Component\Currency\Entity\Interfaces\MoneyInterface;
 use Elcodi\Component\Geo\Entity\Interfaces\AddressInterface;
 use Elcodi\Component\Product\Entity\Interfaces\DimensionableInterface;
+use Elcodi\Component\StateTransitionMachine\Entity\Interfaces\StatefulInterface;
 use Elcodi\Component\User\Entity\Interfaces\CustomerInterface;
 
 /**
  * Class OrderInterface
  */
-interface OrderInterface extends PriceInterface, DimensionableInterface
+interface OrderInterface extends PriceInterface, DimensionableInterface, StatefulInterface
 {
     /**
      * Sets Customer
@@ -95,40 +96,6 @@ interface OrderInterface extends PriceInterface, DimensionableInterface
     public function removeOrderLine(OrderLineInterface $orderLine);
 
     /**
-     * Set order histories
-     *
-     * @param Collection $orderHistories Order histories
-     *
-     * @return $this self Object
-     */
-    public function setOrderHistories(Collection $orderHistories);
-
-    /**
-     * Get order histories
-     *
-     * @return Collection Order histories
-     */
-    public function getOrderHistories();
-
-    /**
-     * Add Order History
-     *
-     * @param OrderHistoryInterface $orderHistory Order History
-     *
-     * @return $this self Object
-     */
-    public function addOrderHistory(OrderHistoryInterface $orderHistory);
-
-    /**
-     * Remove Order History
-     *
-     * @param OrderHistoryInterface $orderHistory Order History
-     *
-     * @return $this self Object
-     */
-    public function removeOrderHistory(OrderHistoryInterface $orderHistory);
-
-    /**
      * Set quantity
      *
      * @param int $quantity Quantity
@@ -143,22 +110,6 @@ interface OrderInterface extends PriceInterface, DimensionableInterface
      * @return integer Quantity
      */
     public function getQuantity();
-
-    /**
-     * Sets LastOrderHistory
-     *
-     * @param OrderHistoryInterface $lastOrderHistory LastOrderHistory
-     *
-     * @return $this Self object
-     */
-    public function setLastOrderHistory(OrderHistoryInterface $lastOrderHistory);
-
-    /**
-     * Get LastOrderHistory
-     *
-     * @return OrderHistoryInterface LastOrderHistory
-     */
-    public function getLastOrderHistory();
 
     /**
      * Gets the Coupon amount with tax

Entity/Interfaces/OrderLineInterface.php
@@ -16,17 +16,16 @@
 
 namespace Elcodi\Component\Cart\Entity\Interfaces;
 
-use Doctrine\Common\Collections\Collection;
-
 use Elcodi\Component\Product\Entity\Interfaces\DimensionableInterface;
 use Elcodi\Component\Product\Entity\Interfaces\ProductInterface;
 use Elcodi\Component\Product\Entity\Interfaces\PurchasableInterface;
 use Elcodi\Component\Product\Entity\Interfaces\VariantInterface;
+use Elcodi\Component\StateTransitionMachine\Entity\Interfaces\StatefulInterface;
 
 /**
  * Class OrderLineInterface
  */
-interface OrderLineInterface extends PriceInterface, DimensionableInterface
+interface OrderLineInterface extends PriceInterface, DimensionableInterface, StatefulInterface
 {
     /**
      * Set Order
@@ -107,56 +106,6 @@ interface OrderLineInterface extends PriceInterface, DimensionableInterface
      * @return integer Quantity
      */
     public function getQuantity();
-
-    /**
-     * Set order line histories
-     *
-     * @param Collection $orderLineHistories Order histories
-     *
-     * @return $this self Object
-     */
-    public function setOrderLineHistories(Collection $orderLineHistories);
-
-    /**
-     * Get order line histories
-     *
-     * @return Collection Order Line histories
-     */
-    public function getOrderLineHistories();
-
-    /**
-     * Add Order History
-     *
-     * @param OrderLineHistoryInterface $orderLineHistory Order History
-     *
-     * @return $this self Object
-     */
-    public function addOrderLineHistory(OrderLineHistoryInterface $orderLineHistory);
-
-    /**
-     * Remove Order History
-     *
-     * @param OrderLineHistoryInterface $orderLineHistory Order Line History
-     *
-     * @return $this self Object
-     */
-    public function removeOrderLineHistory(OrderLineHistoryInterface $orderLineHistory);
-
-    /**
-     * Sets LastOrderLineHistory
-     *
-     * @param OrderLineHistoryInterface $lastOrderLineHistory LastOrderLineHistory
-     *
-     * @return OrderLineInterface Self object
-     */
-    public function setLastOrderLineHistory(OrderLineHistoryInterface $lastOrderLineHistory);
-
-    /**
-     * Get LastOrderLineHistory
-     *
-     * @return OrderLineHistoryInterface LastOrderLineHistory
-     */
-    public function getLastOrderLineHistory();
 
     /**
      * Set the height